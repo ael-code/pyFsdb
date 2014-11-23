d620c33ec90440daf536e4483aefd04f4a0ea86a
ael-code <tommy.ael@gmail.com>
2014-11-23 23:58:58 +0100

    let's see

---
+++ fsdb/__init__.py
@@ -0,0 +1,1 @@
+from Fsdb import Fsdb
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Fsdb",
+    version = "0.1",
+    packages=['fsdb'],
+
+    author = "Ael",
+    author_email = "tommy.ael@gmail.com",
+    description = ("File system database. Easily manage file storing"),
+    keywords = "database file storing db",
+    url = "https://github.com/ael-code/pyFsdb",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Database",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2 :: Only",
+    ],
+)
+
